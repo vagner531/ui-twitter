07dcfd2acb331f10db3c2280cf5b7cf64f2fc8f7
vagner santos <vagner131417@gmail.com>
2023-03-22 10:46:34 -0300

    feat: create global css

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,8 @@
+export function Tweet() {
+  return(
+     <div>
+      Tweet
+      <button>Linke</button>
+     </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import "./global.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <h1>Hello World</h1>
-  </React.StrictMode>,
-)
+
+  </React.StrictMode>
+);
