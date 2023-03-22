c6d3dd894aa88a85e4c877187d4ef5a1a169d84a
vagner santos <vagner131417@gmail.com>
2023-03-22 19:37:32 -0300

    feat: create structure css and main

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-
+    <div className="layout">
+      <aside className="sidebar"></aside>
+      <div className="content">
+        
+      </div>
+    </div>
   </React.StrictMode>
 );
