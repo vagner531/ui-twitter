9fd4bfef1247efda00eea462bc804e0d3b27661d
vagner santos <vagner131417@gmail.com>
2023-03-23 16:49:04 -0300

    feat: create component separator

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./global.css";
 import { Tweet } from "./components/Tweet";
 import { Sidebar } from "./components/Sidebar";
 import { Header } from "./components/Header";
+import { Separator } from "./components/Separator";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -24,11 +25,11 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 
             <button type="submit">Tweet</button>
           </form>
-          <div className="separator" />
+
+          <Separator />
 
           <Tweet />
           <Tweet />
-          
         </main>
       </div>
     </div>
