b6a8cbb6fd855b612d693e5ec0b2aaf57c3b9956
vagner santos <vagner131417@gmail.com>
2023-03-23 16:29:48 -0300

    feat: create component sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,60 @@
+import twitterLogo from "../assets/logo-twitter.svg";
+import {
+  Bell,
+  BookmarkSimple,
+  DotsThreeCircle,
+  Envelope,
+  FileText,
+  Hash,
+  House,
+  User,
+} from "phosphor-react";
+
+import './Sidebar.css';
+
+export function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <img className="logo" src={twitterLogo} alt="logo" />
+
+      <nav className="main-navigation">
+        <a className="active" href="">
+          <House weight="fill" />
+          Home
+        </a>
+        <a href="">
+          <Hash />
+          Explore
+        </a>
+        <a href="">
+          <Bell />
+          Notifications
+        </a>
+        <a href="">
+          <Envelope />
+          Messages
+        </a>
+        <a href="">
+          <BookmarkSimple />
+          Bookmarks
+        </a>
+        <a href="">
+          <FileText />
+          Lists
+        </a>
+        <a href="">
+          <User />
+          Profile
+        </a>
+        <a href="">
+          <DotsThreeCircle />
+          More
+        </a>
+      </nav>
+
+      <button className="new-tweet" type="button">
+        Tweet
+      </button>
+    </aside>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,67 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  Hash,
-  House,
-  Bell,
-  Envelope,
-  BookmarkSimple,
-  FileText,
-  User,
-  DotsThreeCircle,
-  Sparkle,
-} from "phosphor-react";
 
 import "./global.css";
 
-import twitterLogo from "./assets/logo-twitter.svg";
 import { Tweet } from "./components/Tweet";
+import { Sparkle } from "phosphor-react";
+import { Sidebar } from "./components/Sidebar";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar">
-        <img className="logo" src={twitterLogo} alt="logo" />
-
-        <nav className="main-navigation">
-          <a className="active" href="">
-            <House weight="fill" />
-            Home
-          </a>
-          <a href="">
-            <Hash />
-            Explore
-          </a>
-          <a href="">
-            <Bell />
-            Notifications
-          </a>
-          <a href="">
-            <Envelope />
-            Messages
-          </a>
-          <a href="">
-            <BookmarkSimple />
-            Bookmarks
-          </a>
-          <a href="">
-            <FileText />
-            Lists
-          </a>
-          <a href="">
-            <User />
-            Profile
-          </a>
-          <a href="">
-            <DotsThreeCircle />
-            More
-          </a>
-        </nav>
-
-        <button className="new-tweet" type="button">
-          Tweet
-        </button>
-      </aside>
+      <Sidebar />
       <div className="content">
         <main className="timeline">
           <div className="timeline-header">
