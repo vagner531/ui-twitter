e45a84d68cab88e5debecef4d002d3c510c9e77e
vagner santos <vagner131417@gmail.com>
2023-03-22 21:58:36 -0300

    feat: create navbar add phosphor icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { House } from "phosphor-react";
 
 import "./global.css";
 
@@ -11,11 +12,25 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <aside className="sidebar">
         <img className="logo" src={twitterLogo} alt="logo" />
 
-        <nav></nav>
+        <nav className="main-navigation">
+          <a className="active" href="">
+            <House weight="fill" />
+            Home
+          </a>
+          <a href="">Explore</a>
+          <a href="">Notifications</a>
+          <a href="">Messages</a>
+          <a href="">Bookmarks</a>
+          <a href="">Lists</a>
+          <a href="">Profile</a>
+          <a href="">More</a>
+        </nav>
 
-        <button className="new-tweet" type="button">Tweet</button>
+        <button className="new-tweet" type="button">
+          Tweet
+        </button>
       </aside>
       <div className="content">Content</div>
     </div>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
