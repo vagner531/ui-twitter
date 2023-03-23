eb19c45122d6143367fb69b9b52f526279f56276
vagner santos <vagner131417@gmail.com>
2023-03-23 14:51:56 -0300

    feat: create tweet tweet.css

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,8 +1,41 @@
-// export function Tweet() {
-//   return(
-//      <div>
-//       Tweet
-//       <button>Linke</button>
-//      </div>
-//   )
-// }
+import { ArrowsClockwise, ChatCircle, Heart } from "phosphor-react";
+import "./Tweet.css";
+
+export function Tweet() {
+  return (
+    <a href="#" className="tweet">
+      <img src="https://github.com/vagner531.png" alt="" />
+
+      <div className="tweet-content">
+        <div className="tweet-content-header">
+          <strong>Vagner Santos</strong>
+          <span>@vagner531</span>
+        </div>
+        <p>
+          Acabei de migrar um projeto React GIGANTE de create-react-app para
+          Vite e os resultados foram:
+          <br /><br />
+          ✅ npm start: De 32s para 400ms (sim, demorava 30s) <br />
+          ✅ npm build: De 120s para 22s <br />
+          <br />
+          Além disso, troquei do Yarn para o PNPM e o install das deps mudou de
+          24s para 8s 🔥
+        </p>
+        <div className="tweet-content-footer">
+          <button type="button">
+            <ChatCircle />
+             20
+          </button>
+          <button type="button">
+            <ArrowsClockwise />
+             20
+          </button>
+          <button type="button">
+            <Heart />
+             20
+          </button>
+        </div>
+      </div>
+    </a>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ import {
 import "./global.css";
 
 import twitterLogo from "./assets/logo-twitter.svg";
+import { Tweet } from "./components/Tweet";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -77,6 +78,11 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
             <button type="submit">Tweet</button>
           </form>
           <div className="separator" />
+
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
         </main>
       </div>
     </div>
